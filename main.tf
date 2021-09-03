ecc7f412a8145b356e3f5aa74bead572c94f025e
JZ <ee.jzabelin@gmail.com>
2021-09-03 08:39:18 +0000

    change main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server_1" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Netology_test_server"
   }
 }
