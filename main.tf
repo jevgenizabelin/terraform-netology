d92fe269c2b1b64ecfd37cdcdaaee19d8123935b
JZ <ee.jzabelin@gmail.com>
2021-09-07 21:01:26 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,46 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.0"
+ #   }
+#  }
 
-  required_version = ">= 0.14.9"
-}
+#  required_version = ">= 0.14.9"
+#}
+
+#provider "aws" {
+#  profile = "jz-s3"
+#  region  = "us-west-2"
+#}
+
+#resource "aws_instance" "app_server_1" {
+#  ami           = "ami-830c94e3"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "Netology_test_server"
+#  }
+#}
+
 
 provider "aws" {
   profile = "jz-s3"
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "app_server_1" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-  tags = {
-    Name = "Netology_test_server"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
