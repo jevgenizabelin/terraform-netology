0572adffb8ef2143a04fc6f5486c4de959c668a7
JZ <ee.jzabelin@gmail.com>
2022-02-20 20:30:03 +0000

    change main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 3.0"
- #   }
-#  }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+ }
 
-#  required_version = ">= 0.14.9"
-#}
+  required_version = ">= 0.14.9"
+}
 
 #provider "aws" {
 #  profile = "jz-s3"
 #  region  = "us-west-2"
 #}
 
-#resource "aws_instance" "app_server_1" {
-#  ami           = "ami-830c94e3"
-#  instance_type = "t2.micro"
-#
-#  tags = {
-#    Name = "Netology_test_server"
-#  }
-#}
+resource "aws_instance" "app_server_1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Netology_test_server"
+  }
+}
 
 
 provider "aws" {
